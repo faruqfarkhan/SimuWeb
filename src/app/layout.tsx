2ec73fc26334aefa866a3ff33a5c41990b718622
Faruq Farkhan Iskandar <ruqfaruq30@gmail.com>
2025-09-01 07:22:04 +0000

    scriptnya perlu ya? trus saya pindahin gtm di atas ody

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+        <GoogleTagManager gtmId="GTM-WGFPF26Z" />
+      </head>
+      <body className={cn('min-h-screen bg-background font-body antialiased')}>
         {/* Google Tag Manager - Noscript Fallback */}
         <noscript
           dangerouslySetInnerHTML={{
@@ -29,12 +35,6 @@ export default function RootLayout({
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
-      </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased')}>
-        <GoogleTagManager gtmId="GTM-WGFPF26Z" />
         <UserProvider>
           <WishlistProvider>
             <CartProvider>
